38ef2f6d3d6fbafbea27e13db501df6bf4f485a2
Brayan Forero <brayanroswll2802@gmail.com>
2022-01-27 14:29:16 -0400

    add cdn animate.css and estructure card options

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,15 @@
-console.log('hola mundo');
+const $toggleOptions = document.getElementById('toggle-options');
+console.log($toggleOptions);
+const $options = document.getElementById('card-options');
+
+$toggleOptions.addEventListener('click', () => {
+  if ($options.classList.contains('animate__slideInLeft')) {
+    $options.classList.remove('animate__slideInLeft');
+    $options.classList.add('animate__slideOutLeft');
+
+    return;
+  }
+
+  $options.classList.remove('animate__slideOutLeft');
+  $options.classList.add('animate__slideInLeft');
+});
